09aaf1175ffccf621ea43cca01191881b8a56740
saajanrana <saajanrana001@gmail.com>
2023-06-01 14:56:49 +0530

    first commit

---
+++ src/comoponents/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const  Footer = ()=>{
+  return (
+    <footer>
+        <div>
+            <h1>TheOne</h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow us</h5>
+            <div>
+              <a href="https://instagram.com/saajan_rana1" target={"blank"}>Instagram</a> 
+              <a href="https://github.com/saajanrana" target={"blank"}>GitHub</a>
+
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/comoponents/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import { HashLink } from 'react-router-hash-link'; 
+
+export default function Header() {
+  return (
+    <nav>
+        <h1>TheOne.</h1>
+        <main>
+<HashLink to = {"/#home"}>Home</HashLink>
+<Link to = {"/contact"}>Contact</Link>
+<HashLink to = {"/#about"}>about</HashLink>
+<HashLink to = {"/#brands"}>Brands</HashLink>
+<Link to = {"/services"}>Services</Link>
+        </main>
+    </nav>
+  )
+}
